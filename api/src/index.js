c10a0fb4790ed71f7b96bf1cf57fe777758f0dae
CanMeng <cmcmail@vip.qq.com>
2020-06-18 22:09:19 +0800

    demov1

---
+++ api/src/index.js
@@ -0,0 +1,44 @@
+
+/**************************************************
+Node8.9-Mysql
+Reference: mysql api---https://www.npmjs.com/package/mysql
+Reference: How to access database---https://cloud.tencent.com/document/product/236/3130
+Reference: How to connect api gateway with scf---https://cloud.tencent.com/document/product/628/11983
+***************************************************/
+
+function wrapPromise(connection, sql) {
+  return new Promise((res, rej) => {
+    connection.query(sql, function(error, results, fields) {
+      if (error) {
+        rej(error)
+      }
+      res(results)
+    })
+  })
+}
+
+
+exports.main_handler = async (event, context, callback) => {
+  const mysql = require('mysql');
+  const connection = mysql.createConnection({
+    host     : 'gz-cdb-j1ju5xot.sql.tencentcdb.com',       
+    user     : 'root',              
+    password : 'serverless@2019',       
+    port: '60454',                   
+    database: 'serverless_form'
+  });
+
+  connection.connect();
+  console.log(event.body)
+
+  // get value from apigw
+  const { username, email, phone, company, poi, company_size, message} = JSON.parse(event.body)
+
+  const updateSql = `INSERT INTO user_slsdays ( username,email,phone,company,use_status,company_size,comment,created_time ) VALUES ( '${username}','${email}','${phone}','${company}',${poi},${company_size},'${message}',NOW() )`
+
+  let queryResult = await wrapPromise(connection, updateSql)
+  
+  connection.end();
+
+  return queryResult
+}
---
+++ web/.eleventy.js
@@ -0,0 +1,73 @@
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt()
+
+const Purgecss = require('purgecss')
+const { JSDOM } = require('jsdom')
+const CleanCSS = require("clean-css");
+
+const cssFiles = ['./src/css/custom.css','./src/css/markdown.css', './src/css/tachyons.css']
+
+const cleanCSSOptions = {
+  level: {
+    2: {
+      all: true, 
+      removeDuplicateRules: true 
+    }
+  }
+}
+
+const insertCss = (html, css) => {
+
+    const dom = new JSDOM(html)
+    
+    const { document } = dom.window
+
+    let head = document.getElementsByTagName('head')[0];
+    let style = document.createElement("style");
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+
+    return dom.serialize()
+}
+
+
+module.exports = function (eleventyConfig) {
+    // Copy the `img/` directory
+    eleventyConfig.addPassthroughCopy('src/img')
+    eleventyConfig.addPassthroughCopy('src/static')
+  
+    // Copy the `css/fonts/` directory
+    // If you use a subdirectory, it’ll copy using the same directory structure.
+    // eleventyConfig.addPassthroughCopy('src/css')
+    eleventyConfig.addNunjucksShortcode("markdown", function(text) { return md.render(text) }); 
+    
+    eleventyConfig.addTransform("purgeCSS", function(content, outputPath){
+      if( outputPath.endsWith(".html") ) {
+        console.log(outputPath)
+        const purgecss = new Purgecss({
+          content: [outputPath],
+          css: cssFiles
+        })
+        const purgecssResult = purgecss.purge()
+        let cssMerge = ''
+        if(purgecssResult.length>0){
+          for (let i = 0; i < purgecssResult.length; i++){
+            cssMerge= cssMerge.concat(purgecssResult[i].css)
+          }
+          const cssMin = new CleanCSS(cleanCSSOptions).minify(cssMerge).styles
+          return insertCss(content, cssMin)
+        }
+      }
+      return content
+
+    })
+
+    return {
+      passthroughFileCopy: true,
+      dir: {
+        input: 'src'
+      },
+      markdownTemplateEngine: 'njk'
+    }
+  }
